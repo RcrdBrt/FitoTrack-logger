871d9abf982f0218623f5fceca5d28fa889242ab
Riccardo Berto <riccardobrt@gmail.com>
2021-02-23 16:27:03 +0100

    mail login

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ from sqlalchemy import create_engine
 
 config = configparser.ConfigParser().read('config.ini')
 db = create_engine(f"postgresql://{config['db']['username']}:{config['db']['password']}@{config['db']['host']}/{config['db']['database']}")
+mail = IMAP4(host=config['mail']['host'])
+
+
+def get_gpx_files_from_mail():
+    mail.login(config['mail']['username'], config['mail']['password'])
+
+    mail.logout()
 
 
 def main():
