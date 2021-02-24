ecd763dad25da371339078ff1d34fe2afd18b5bd
Riccardo Berto <riccardobrt@gmail.com>
2021-02-24 10:36:04 +0100

    filename column in training table to prevent duplicates

--- init.sql
+++ init.sql
@@ -1,5 +1,6 @@
 create table if not exists training(
     id uuid primary key default gen_random_uuid(),
+    filename text not null,
     medium varchar(255) not null,
     comment text not null,
     data jsonb not null default '{}'
