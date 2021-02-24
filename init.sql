800d5151b65a78915c3e818ddf340e6788493710
Riccardo Berto <riccardobrt@gmail.com>
2021-02-24 14:00:37 +0100

    removed distance & kcal col from training_data

--- init.sql
+++ init.sql
@@ -18,8 +18,6 @@ create table if not exists training_data(
     lon float not null,
     speed float not null,
     elevation float not null,
-    distance float not null,
-    kcal int not null,
     constraint fk_training_id foreign key(training_id) references training(id) on delete cascade
 );
 create index if not exists idx_training_data_training_id on training_data(training_id);
