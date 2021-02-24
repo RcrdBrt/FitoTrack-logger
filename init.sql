e4df2727e6bad98cdfbe706fdb31bf65bea6d0ce
Riccardo Berto <riccardobrt@gmail.com>
2021-02-24 16:08:21 +0100

    type instead of medium

--- init.sql
+++ init.sql
@@ -2,7 +2,7 @@ create table if not exists training(
     id uuid primary key default gen_random_uuid(),
     owner varchar(255) not null,
     filename text not null,
-    medium varchar(255) not null,
+    type varchar(255) not null,
     description text not null,
     moving_time float not null,
     stopped_time float not null,
