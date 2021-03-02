aa792ce33aacb43f0dab2379f3e03794435ce426
Riccardo Berto <riccardobrt@gmail.com>
2021-03-02 09:00:13 +0100

    training_info view

--- init.sql
+++ init.sql
@@ -48,3 +48,17 @@ from (select td_row_n.t, td_row_n.id, td_row_n.geog
 
 	on (td_start.id = td_end.id)
 );
+
+create or replace view training_info as
+(select distinct
+        td.t::date,
+        date_trunc('second', training_duration.start_time::time) as start_time,
+        date_trunc('second', training_duration.end_time::time) as end_time,
+        training_duration.duration as duration,
+        round(((t.moving_distance)/1000)::numeric, 2) as distance,
+        round(( (t.moving_distance / 1000) / (select extract(epoch from training_duration.duration)/3600))::numeric, 1) as pace_kmh
+    from training t
+        join training_data td on (t.id = td.training_id)
+        join training_duration on (t.id = training_duration.id)
+order by td.t::date, date_trunc('second', training_duration.start_time::time) asc
+);
