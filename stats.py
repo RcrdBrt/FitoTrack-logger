b9a575219abeccff7c2e91be671dd869c9fe1c1e
Riccardo Berto <riccardobrt@gmail.com>
2021-03-02 09:00:57 +0100

    overall stats as training_info view

--- stats.py
+++ stats.py
@@ -3,17 +3,8 @@ from tabulate import tabulate
 
 def print_stats(db: Connection):
     overall_stats = """
-        select distinct
-		        td.t::date,
-		        date_trunc('second', training_duration.start_time::time) as start_time,
-		        date_trunc('second', training_duration.end_time::time) as end_time,
-                training_duration.duration as duration,
-		        round(((t.moving_distance)/1000)::numeric, 2) as distance,
-		        round(( (t.moving_distance / 1000) / (select extract(epoch from training_duration.duration)/3600))::numeric, 1) as pace_kmh
-	        from training t
-		        join training_data td on (t.id = td.training_id)
-		        join training_duration on (t.id = training_duration.id)
-        order by td.t::date, date_trunc('second', training_duration.start_time::time) asc
+        select *
+            from training_info
     """
 
     print(tabulate(list(db.execute(overall_stats)),
